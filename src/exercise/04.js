4dac46ddfecb63b29812ca941984c626ebfe9a7b
Ana Morales <anamorales.dev@gmail.com>
2023-10-24 10:48:54 -0400

    Complete exercise-4 Props Collections and Getters

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -4,23 +4,70 @@
 import * as React from 'react'
 import {Switch} from '../switch'
 
+/**
+ * A function that will accept any number of functions
+ * and returns a function that accepts any number of arguments
+ */
+const callAll = (...fns) => {
+  return (...args) => {
+    fns.forEach(fn => {
+      fn && fn(...args)
+    })
+  }
+}
+
 function useToggle() {
   const [on, setOn] = React.useState(false)
   const toggle = () => setOn(!on)
 
-  // 🐨 Add a property called `togglerProps`. It should be an object that has
-  // `aria-pressed` and `onClick` properties.
-  // 💰 {'aria-pressed': on, onClick: toggle}
-  return {on, toggle}
+  // This is the props collection strategy
+  // const togglerProps = {
+  //   'aria-pressed': on,
+  //   onClick: toggle,
+  // }
+  // return {on, toggle, togglerProps}
+
+  const getTogglerProps = ({onClick, ...props} = {}) => {
+    return {
+      'aria-pressed': on,
+      // onClick: () => {
+      //   onClick && onClick()
+      //   toggle()
+      // },
+      onClick: callAll(onClick, toggle),
+      ...props,
+    }
+  }
+
+  return {on, toggle, getTogglerProps}
 }
 
+// function App() {
+//   const {on, togglerProps} = useToggle()
+//   return (
+//     <div>
+//       <Switch on={on} {...togglerProps} />
+//       <hr />
+//       <button aria-label="custom-button" {...togglerProps}>
+//         {on ? 'on' : 'off'}
+//       </button>
+//     </div>
+//   )
+// }
+
+// Extra Credit - Prop Getters is the recommended strategy!
 function App() {
-  const {on, togglerProps} = useToggle()
+  const {on, getTogglerProps} = useToggle()
   return (
     <div>
-      <Switch on={on} {...togglerProps} />
+      <Switch {...getTogglerProps({on})} />
       <hr />
-      <button aria-label="custom-button" {...togglerProps}>
+      <button
+        {...getTogglerProps({
+          'aria-label': 'custom-button',
+          onClick: () => console.info('onButtonClick'),
+        })}
+      >
         {on ? 'on' : 'off'}
       </button>
     </div>
